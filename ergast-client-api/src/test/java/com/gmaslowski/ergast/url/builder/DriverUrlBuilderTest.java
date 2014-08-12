2eedd71461a68581df4343f88ccbd0c0a7f34a1a
Grzegorz Maslowski <gmaslowski@future-processing.com>
2014-08-12 13:12:15 +0200

    Added constructor url creation support.

--- ergast-client-api/src/test/java/com/gmaslowski/ergast/url/builder/DriverUrlBuilderTest.java
+++ ergast-client-api/src/test/java/com/gmaslowski/ergast/url/builder/DriverUrlBuilderTest.java
@@ -2,29 +2,26 @@ package com.gmaslowski.ergast.url.builder;
 
 import static com.gmaslowski.ergast.url.builder.DriverUrlBuilder.driver;
 import static com.gmaslowski.ergast.url.testdata.DriverTestData.ALONSO;
-import static org.fest.assertions.Assertions.assertThat;
-import com.gmaslowski.ergast.test.AbstractUnitTest;
-import org.junit.Test;
 
-public class DriverUrlBuilderTest extends AbstractUnitTest {
+public class DriverUrlBuilderTest extends AbstractUrlBuilderTest {
 
-    private String path;
-
-    @Test
-    public void shouldAppendDriversPathToUrl() {
-        // when
-        path = driver().path();
-
-        // then
-        assertThat(path).contains("/drivers");
+    @Override
+    String givenPath() {
+        return driver().path();
     }
 
-    @Test
-    public void shouldAppendDriversPathWithGivenDriverToUrl() {
-        // when
-        path = driver(ALONSO).path();
+    @Override
+    String expectedPath() {
+        return "/drivers";
+    }
 
-        // then
-        assertThat(path).contains("/drivers/alonso");
+    @Override
+    String givenPathWithValue() {
+        return driver(ALONSO).path();
+    }
+
+    @Override
+    String expectedPathWithValue() {
+        return "/drivers/alonso";
     }
 }
